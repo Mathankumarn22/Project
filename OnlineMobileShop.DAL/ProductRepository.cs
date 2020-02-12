c28328c5a0e5060cb1467b93c959468890185a5a
Mathankumar <mathankumarmn22@gmail.com>
2020-02-12 23:29:24 +0000

    commit

--- OnlineMobileShop.DAL/ProductRepository.cs
+++ OnlineMobileShop.DAL/ProductRepository.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using OnlineMobileShop.Entity;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
